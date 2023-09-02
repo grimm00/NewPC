d6656fe8f0a8d391971e30a4fd4512c65842de8d
Grimm00 <cdwilson001@gmail.com>
2023-09-02 09:29:27 -0500

    Refactor: modified customerinstaller
    - Removed "TaskBar" from install options.
    - Removed csv file mentioned in personal directory and pointed to new software csv file
    in working directory
    - $workdir was changed to $installdir and this directory will be called
    explicitly, removing need to move from working directory.

--- custominstaller.ps1
+++ custominstaller.ps1
@@ -1,15 +1,13 @@
-$workdir = "C:\installer"
+$installdir = "C:\installer"
 
-If (Test-Path -Path $workdir -PathType Container)
+If (Test-Path -Path $installdir -PathType Container)
 { Write-Host "$workdir already exists" -ForegroundColor Red}
 ELSE
-{ New-Item -Path $workdir -ItemType directory }
-
-cd $workdir
+{ New-Item -Path $installdir -ItemType directory }
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-$Downloads = Import-Csv -Path C:\Users\grimm\Documents\onlinesoftware.csv
+$Downloads = Import-Csv -Path ./downloadfiles.csv 
 
 if ($Downloads) {
     Write-Host "CSV file loaded successfully."
@@ -18,43 +16,39 @@ if ($Downloads) {
 }
 
 $programs = @(
-		@{'Name' = 'googlechrome'; 'installer' = 'chocolatey'; 'Taskbar' = 'Yes'},
-		@{'Name' = 'notepadplusplus'; 'installer' = 'chocolatey'; 'Taskbar' = 'No'},
-		@{'Name' = 'python3'; 'installer' = 'chocolatey'; 'Taskbar' = 'No'},
-		@{'Name' = 'firefox'; 'installer' = 'chocolatey'; 'Taskbar' = 'No'},
-		@{'Name' = 'vscode'; 'installer' = 'chocolatey'; 'Taskbar' = 'Yes'},
-		@{'Name' = 'git'; 'installer' = 'chocolatey'; 'Taskbar' = 'Yes'}
-		@{'Name' = 'virtualbox'; 'installer' = 'chocolatey'; 'Taskbar' = 'Yes'}
-		@{'Name' = 'cpu-z'; 'installer' = 'chocolatey'; 'Taskbar' = 'No'}
-		@{'Name' = 'r'; 'installer' = 'chocolatey'; 'Taskbar' = 'No'}
-		@{'Name' = 'rtools'; 'installer' = 'chocolatey'; 'Taskbar' = 'No'}
-		@{'Name' = 'r.studio'; 'installer' = 'chocolatey'; 'Taskbar' = 'No'}
-		@{'Name' = 'sqlitebrowser'; 'installer' = 'chocolatey'; 'Taskbar' = 'No'}
-		@{'Name' = 'discord'; 'installer' = 'chocolatey'; 'Taskbar' = 'No'}
+		@{'Name' = 'googlechrome'; 'installer' = 'chocolatey'},
+		@{'Name' = 'notepadplusplus'; 'installer' = 'chocolatey'},
+		@{'Name' = 'python3'; 'installer' = 'chocolatey'},
+		@{'Name' = 'firefox'; 'installer' = 'chocolatey'},
+		@{'Name' = 'vscode'; 'installer' = 'chocolatey'},
+		@{'Name' = 'git'; 'installer' = 'chocolatey'}
+		@{'Name' = 'virtualbox'; 'installer' = 'chocolatey'}
+		@{'Name' = 'cpu-z'; 'installer' = 'chocolatey'}
+		@{'Name' = 'r'; 'installer' = 'chocolatey'}
+		@{'Name' = 'rtools'; 'installer' = 'chocolatey'}
+		@{'Name' = 'r.studio'; 'installer' = 'chocolatey'}
+		@{'Name' = 'sqlitebrowser'; 'installer' = 'chocolatey'}
+		@{'Name' = 'discord'; 'installer' = 'chocolatey'}
 )
 foreach ($program in $programs) {  
 
 	Write-Host "Installing: $($program['Name'])" 
 	
 	IF ($program['installer'] -eq 'chocolatey') {
-		#Write-Host "This will be chocolate installed"
-		#choco install $($program['Name']) -y
+		
+		# This will be chocolate installed
+		choco install $($program['Name']) -y
+		
 	} ELSEIF ($program['installer'] -eq 'download') {
+		#
 		foreach ($Download in $Downloads) {
 			if ($program['Name'] -eq $Download.Name) {
-			$Outfile = ("{0}\{1}.{2}" -f $workdir, $Download.Name, $Download.FileType)
+			$Outfile = ("{0}\{1}.{2}" -f $installdir, $Download.Name, $Download.FileType)
 			Invoke-WebRequest $Download.Link -OutFile $Outfile
-			Write-Host "$($Download.Name), $($Download.Link), $($Download.FileType)"
 			Write-Host $Outfile
 			}
 		}
 	}
-	
-	IF ($program['Taskbar'] -eq 'Yes') {
-		#Write-Host "This will have a taskbar"
-	} ELSEIF ($program['Taskbar'] -eq 'No') {
-		#Write-Host  "This will not have a taskbar"
-	} 
 	
 } 
 
