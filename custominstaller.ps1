ccf98f59910eb795715fee648a3c291d7bdea27a
Grimm00 <cdwilson001@gmail.com>
2023-09-02 21:49:58 -0500

    Refactor: First version release. v1.0.1-beta
    - Added Synopsis, Description and Notes
    - Changed install directory name and the variable assigned
    - Removed list of programs, having the program iterate through softwareinstallers.csv
    - Cleaned up code assigned to variables
    - Program now checks if downloaded installer exists before attempting download.

--- custominstaller.ps1
+++ custominstaller.ps1
@@ -1,56 +1,61 @@
-$installdir = "C:\installer"
+<#
+.SYNOPSIS
+    PowerShell script designed to automate the installation of multiple software packages on a Windows machine.
+.DESCRIPTION
+    The script leverages the popular package manager Chocolatey, as well as direct download links where necessary, to streamline the setup process. This script is particularly useful for setting up new machines, or in environments where multiple machines need to have the same set of applications installed. 
+.NOTES
+    File Name      : custominstaller.ps1
+    Author         : Cam Wilson
+    Date           : 09/02/2023
+    Last Modified  : 09/02/2023
+    Version        : 1.0.1-beta
+#>
 
-If (Test-Path -Path $installdir -PathType Container)
+#Creates "custom_installers" folder in root directory (e.g. C:\custom_installers)
+$installerdir = "$env:SystemDrive\custom_installers"
+
+If (Test-Path -Path $installerdir -PathType Container)
 { Write-Host "$workdir already exists" -ForegroundColor Red}
 ELSE
-{ New-Item -Path $installdir -ItemType directory }
+{ New-Item -Path $installerdir -ItemType directory }
 
+#Downloads and Runs PowerShell script to install Chocolatey Package Manager
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-$Downloads = Import-Csv -Path ./downloadfiles.csv 
+#Loads softwareinstallers.csv file. This lists software name, type of installation, and link. 
+$Softwares = Import-Csv -Path ./softwareinstallers.csv 
 
-if ($Downloads) {
+if ($Softwares) {
     Write-Host "CSV file loaded successfully."
 } else {
     Write-Host "Failed to load CSV file."
 }
 
-$programs = @(
-		@{'Name' = 'googlechrome'; 'installer' = 'chocolatey'},
-		@{'Name' = 'notepadplusplus'; 'installer' = 'chocolatey'},
-		@{'Name' = 'python3'; 'installer' = 'chocolatey'},
-		@{'Name' = 'firefox'; 'installer' = 'chocolatey'},
-		@{'Name' = 'vscode'; 'installer' = 'chocolatey'},
-		@{'Name' = 'git'; 'installer' = 'chocolatey'}
-		@{'Name' = 'virtualbox'; 'installer' = 'chocolatey'}
-		@{'Name' = 'cpu-z'; 'installer' = 'chocolatey'}
-		@{'Name' = 'r'; 'installer' = 'chocolatey'}
-		@{'Name' = 'rtools'; 'installer' = 'chocolatey'}
-		@{'Name' = 'r.studio'; 'installer' = 'chocolatey'}
-		@{'Name' = 'sqlitebrowser'; 'installer' = 'chocolatey'}
-		@{'Name' = 'discord'; 'installer' = 'chocolatey'}
-)
-foreach ($program in $programs) {  
-
-	Write-Host "Installing: $($program['Name'])" 
+#Iterates through csv, either installing via chocolatey or downloading installer for manual download. 
+foreach ($Software in $Softwares) {  
 	
-	IF ($program['installer'] -eq 'chocolatey') {
+	IF ($Software.Installer -eq 'chocolatey') {
+		Write-Host "Installing: $($Software.Name)"
+		Write-Host $($Software.Link) will be chocolate installed.
+		choco install $Software.Name -y
 		
-		# This will be chocolate installed
-		choco install $($program['Name']) -y
+	} ELSEIF ($Software.Installer -eq 'download') {
+		$Link = $Software.Link
+		$FileName = $Link.split("/")[-1]
+		$Outfile = "$($installerdir)\$($FileName)"
 		
-	} ELSEIF ($program['installer'] -eq 'download') {
-		#
-		foreach ($Download in $Downloads) {
-			if ($program['Name'] -eq $Download.Name) {
-			$Outfile = ("{0}\{1}.{2}" -f $installdir, $Download.Name, $Download.FileType)
-			Invoke-WebRequest $Download.Link -OutFile $Outfile
-			Write-Host $Outfile
+		#Checks if file exists
+		If (Test-Path $Outfile) {
+			Write-Host "Installer already exists: $FileName"
+		} ELSE {
+			Invoke-WebRequest $Software.Link -OutFile $Outfile
+			If (Test-Path $Outfile) {
+			Write-Host "$($FileName) successfully downloaded for manual install."
+				}
 			}
 		}
-	}
-	
-} 
+	} 
 
+#Sets new programs to system PATH.
 $env:PATH = $env:PATH + ";C:\Program Files\Oracle\VirtualBox" #For VBoxManage commands
 $env:PATH = $env:PATH + ";C:\Program Files\Git\bin" #For Git
